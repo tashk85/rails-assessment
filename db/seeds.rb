bef4cc86de02068d329bf1ad685aa2c41b6e9fbc
Luke Turner <luke@lukedturner.com>
2019-04-30 17:40:12 +1000

    Added printer to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,59 @@ if Listing.count == 0
     end
     puts "Successfully created #{count} listings"
 end
+
+
+if Printer.count == 0
+    printers = []
+    count = 0
+    @models = [
+        "SuperTurbo Mach IV",
+        "Pretty Good Printer (TM)",
+        "Piece Of Trash Printer",
+        "HyperSpace Accellerator 13"
+    ]
+
+
+    for i in 2..11
+        if User.find(i).user_type == "printer"
+            printer = Printer.create(
+                user_id: User.find(i).id,
+                abn: Random.rand(100000000..999999999),
+                printer_model: @models[Random.rand(0..@models.length)],
+                )
+                printers.push(printers)
+                puts "Created printer entry for user_id #{i}"
+                count +=1
+        end
+    end
+
+    puts "Successfully created #{count} listings"
+
+
+end
+
+
+
+
+
+# if Quote.count == 0
+#     quotes = []
+#     count = 0
+    
+#     for i in 2..11
+#         quote = Quote.create(
+#             printer_id: Random.rand(2..11),
+#             listing_id: Random.rand(Listing.first.id..Listing.last.id),
+#             # Listing.find(Random.rand(Listing.first..Listing.last)),
+#             has_job: false,
+#             total_price: Random.rand(100..500).to_f,
+#             job_size: Random.rand(5..200),
+#             turnaround_time: Faker::Date.forward(Random.rand(1..14))
+#             )
+#             quotes.push(quote)
+#             puts "Created quote "
+
+#         count +=1
+#     end
+#     puts "Successfully created quotes"
+# end
