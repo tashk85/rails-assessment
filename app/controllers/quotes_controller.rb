c1d18adfa2fa4148e0fe0861f0cb36092842749c
Natasha <tashk85@gmail.com>
2019-05-01 15:28:48 +1000

    added quotes methods in controller and views

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,17 +1,27 @@
 class QuotesController < ApplicationController
     def index
-        #see all quotes
+        #shows all quotes
+        @quotes = Quote.all
     end
 
     def show
-        # view a single quote
+        # view a single quotev
+        id = params[:id]
+        @quote = Quote.find(id)
     end
 
     def new
         # shows form for creating a new quote
+        @quote = Quote.new
     end
 
     def edit
         # shows form for editing an existing quote
+        @quote = Quote.find(params[:id])
+    end
+
+    def update
+        # updates the listing
+        
     end
 end
