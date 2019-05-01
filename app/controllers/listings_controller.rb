cb4e74676d4acf1c81a40ac6dce5e831b7f3131f
Luke Turner <luke@lukedturner.com>
2019-05-01 14:01:47 +1000

    Added 4 views for listings, and 2 for pages

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,17 +1,13 @@
 class ListingsController < ApplicationController
     def index
-        render plain: "This will show all listings"
     end
 
     def show
-        render plain: "This will show a specific listing"
     end
 
     def new
-        render plain: "This will show a new listing form"
     end
 
     def edit
-        render plain: "This will show the edit listing form"
     end
 end
