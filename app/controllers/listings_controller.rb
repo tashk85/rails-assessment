5753334ff0d2330d1c4cb6c8fbf730fc98ad28df
Luke Turner <luke@lukedturner.com>
2019-05-01 14:50:22 +1000

    Added post route for new listing, and made a form partial

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -10,4 +10,24 @@ class ListingsController < ApplicationController
 
     def edit
     end
+
+    def create
+        #create new listing
+        @listing = current_user.listings.create(listing_params)
+        
+        if @listing.errors.any?
+            # set_breeds_and_sexes
+            render "new"
+        else
+            redirect_to listings_path
+        end
+    end
+
+
+
+    private
+
+    def listing_params
+        params.require(:listing).permit(:title, :description, :budget, :due_date, :file)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,10 @@ Rails.application.routes.draw do
 
   get "/listings", to: "listings#index", as: "listings"
   get "/listings/new", to: "listings#new", as: "new_listing"
-
   get "/listings/:id", to: "listings#show", as: "listing"
   get "/listings/:id/edit", to: "listings#edit", as: "edit_listing"
+  
+  post "/listings/new", to: "listings#create", as: "create_listing"
 
   ### Quotes ###
 
