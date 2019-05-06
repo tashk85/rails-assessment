194825e7282ad100864ddc3652281f7f52c8de56
Natasha <tashk85@gmail.com>
2019-05-06 10:42:44 +1000

    added jobs views and methods in controller.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,12 @@
 class JobsController < ApplicationController
     def index 
-        render plain: "This will show all jobs"
+        # render plain: "This will show all jobs"
+        @jobs = Job.all
     end
 
     def show
-        render plain: "This will show a specific job"
+        # render plain: "This will show a specific job"
+        id = params[:id]
+        @job = Job.find(id)
     end
 end
