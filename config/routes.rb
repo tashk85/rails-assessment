7aad916c61552e76347efd61fdf67b6f719d7e8b
Luke Turner <luke@lukedturner.com>
2019-05-09 16:01:33 +1000

    Changed routes back, this was not the issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   #use custom registrations controller
   devise_for :users, path: "/", path_names: {sign_in: "login", sign_up: "signup"}, controllers: {:registrations => "registrations"}
   # get "/", to: "pages#index", as: "root"
-  # root to: "pages#index"
-  get "/", to: "pages#index", as: "root"
+  root to: "pages#index"
+  # get "/", to: "pages#index", as: "root"
   get "/dashboard", to: "pages#dashboard", as: "dashboard"
 
   #extra information for printers to enter:
